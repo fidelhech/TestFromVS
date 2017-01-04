7621a8f026e35b582ba915ebaab88cadb1877706
Fidel Hechavarria <fidelhech@gmail.com>
2017-01-03 21:56:28 -0500

    First commit!

---
+++ TestFromVS/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TestFromVS
+{
+    class Program
+    {
+        static void Main()
+        {
+            double runningTotal = 0;
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+                // Prompt user for minutes exercised 
+                Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+
+                string entry = Console.ReadLine();
+
+                if (entry.ToLower() == "quit")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    // Add minutes exercised to total 
+                    try
+                    {
+                        double minutes = double.Parse(entry);
+
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine(minutes + " is not an acceptable value.");
+                        }
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("Better than nothing, am I right?");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Way to go hot stuff!");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("You must be a ninja warrior in training!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Okay, now you're just showing off!");
+                        }
+
+                        runningTotal += minutes;
+
+                        // Display total minutes exercised to the screen
+                        Console.WriteLine("You've entered " + runningTotal + " minutes");
+
+                        // Repeat until user quits 
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("This is not a valid option");
+                        continue;
+                    }
+
+                }
+
+            }
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
